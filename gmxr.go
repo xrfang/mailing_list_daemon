c10ae550ef82489c6bbdf45a60abd19fd1c71651
Shannon Fang <xrfang@gmail.com>
2012-10-12 17:37:14 +0800

    MX resolver sample code (to be integrated)

---
+++ gmxr.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "net"
+    "os"
+    "fmt"
+    "strings"
+    "bufio"
+)
+
+func MXResolver(ctrl chan chan string) {
+    for {
+        ch := <- ctrl
+        go func(c chan string) {
+            domain := <-c
+            mx, err := net.LookupMX(domain)
+            if err == nil {
+                cnt := 0
+                for i := 0; i < len(mx); i++ {
+                    ips, err := net.LookupIP(mx[i].Host)
+                    if err == nil {
+                        for _, ip := range ips {
+                            c <- "@" + ip.String()
+                            cnt++
+                        }
+                    }
+                }
+                if cnt == 0 {
+                    c <- "!Cannot get MX record for " + domain
+                }
+            } else {
+                c <- "!" + err.Error()
+            }
+            close(c)
+        }(ch)
+    }
+}
+
+func main() {
+    mxrc := make(chan chan string)
+    go MXResolver(mxrc)
+    fmt.Println("Go MX Resolver Ready")
+    br := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print("> ")
+        input, _, err := br.ReadLine()
+        if err != nil {
+            panic(err)
+        }
+        name := strings.ToLower(strings.TrimSpace(string(input)))
+        if name == "quit" || name == "exit" {
+            break
+        }
+        ch := make(chan string)
+        mxrc <- ch
+        ch <- name
+        for {
+            ip, ok := <-ch
+            if !ok {
+                break
+            }
+            if ip[0] == '@' {
+                fmt.Println(ip[1:])
+            } else {
+                fmt.Println("ERROR: " + ip[1:])
+            }
+        }
+    }
+}
