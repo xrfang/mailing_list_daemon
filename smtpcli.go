09518a3aa53547dbc290809a84d55436008974bf
Xiangrong Fang <xrfang@gmail.com>
2012-10-21 21:44:30 +0800

    Modified smtp client sample code

--- smtpcli.go
+++ smtpcli.go
@@ -1,7 +1,28 @@
 package main
 
 import (
+    "bufio"
+	"net"
+	"os"
+	"fmt"
+	"time"
 )
 
 func main() {
+    conn, err := net.Dial("tcp", os.Args[1] + ":25")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected")
+	conn.SetDeadline(time.Now().Add(10 * time.Second))
+	br := bufio.NewReader(conn)
+	for {
+		msg, _, err := br.ReadLine()
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		fmt.Println(string(msg))
+	}
+	conn.Close()
 }
