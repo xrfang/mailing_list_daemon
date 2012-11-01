4e3e9986dbfa82c7f1b82a5699a3ad5ecb35c1e1
Shannon Fang <xrfang@gmail.com>
2012-11-01 15:27:33 +0800

    Refined debug information for messages put on hold

--- src/smtp/envelope.go
+++ src/smtp/envelope.go
@@ -39,7 +39,8 @@ func loadEnvelope(file string, ss *Settings) *envelope {
 	}
 	now := time.Now().Unix()
 	if ts > now {
-		ss.Debug("On hold: " + path.Base(file))
+		until := time.Unix(ts, 0)
+		ss.Debugf("[%s] on hold until %s", path.Base(file), until.Format("%Y-%m-%d %H:%M:%S"))
 		return nil //scheduled time for this mail is not reached yet
 	}
 	ef, err := os.OpenFile(file, os.O_RDWR, 0600)
